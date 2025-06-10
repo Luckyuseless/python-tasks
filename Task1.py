fe538f6c15e03c12cdeb8e43221f4e2697452f9b
Lakshya jain <lakshyajain688@gmail.com>
2025-06-10 14:28:30 +0530

    Delete Task1.py

--- Task1.py
+++
@@ -1,7 +0,0 @@
-num_1=int(input("Enter the first number: "))
-num_2=int(input("Enter the second number: "))
-print()
-print("Addition:", num_1+num_2)
-print("Multiplication:", num_1*num_2)
-print("Subtraction:", num_1-num_2)
-print("Division:", num_1/num_2)
